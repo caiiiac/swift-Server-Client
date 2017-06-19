aebf3f739bd3bba090bf5d574270ee82368eeab7
caiiiac <caiiiac@163.com>
2017-06-19 10:39:01 +0800

    init server

---
+++ Server/Server/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Server
+//
+//  Created by 唐三彩 on 2017/6/19.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
