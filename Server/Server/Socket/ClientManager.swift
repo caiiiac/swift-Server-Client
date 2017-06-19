05e7096b039102e5382191cd3859057fcfdbf297
caiiiac <caiiiac@163.com>
2017-06-19 10:50:21 +0800

    add manager

---
+++ Server/Server/Socket/ClientManager.swift
@@ -0,0 +1,13 @@
+//
+//  ClientManager.swift
+//  Server
+//
+//  Created by 唐三彩 on 2017/6/19.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import Cocoa
+
+class ClientManager: NSObject {
+
+}
---
+++ Server/Server/Socket/ServerManager.swift
@@ -0,0 +1,13 @@
+//
+//  ServerManager.swift
+//  Server
+//
+//  Created by 唐三彩 on 2017/6/19.
+//  Copyright © 2017年 唐三彩. All rights reserved.
+//
+
+import Cocoa
+
+class ServerManager: NSObject {
+
+}
