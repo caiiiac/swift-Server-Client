0c1fa6e53bc304d25f1520f5dc0071c36786e5e0
caiiiac <caiiiac@163.com>
2017-06-19 10:51:12 +0800

    简单连接

--- Server/Server/ViewController.swift
+++ Server/Server/ViewController.swift
@@ -9,19 +9,21 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var hintLabel: NSTextField!
+    fileprivate lazy var serverMgr : ServerManager = ServerManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    
+    @IBAction func startServer(_ sender: NSButton) {
+        serverMgr.startRunning()
+        hintLabel.stringValue = "服务器已经开启ing"
     }
-
-
+    
+    @IBAction func stopServer(_ sender: NSButton) {
+        serverMgr.stopRunning()
+        hintLabel.stringValue = "服务器未开启"
+    }
 }
-
