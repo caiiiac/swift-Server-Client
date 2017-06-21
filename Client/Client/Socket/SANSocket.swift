1d9994cb4703ee9bf489d437aca9e6d4eb680778
caiiiac <caiiiac@163.com>
2017-06-21 22:20:25 +0800

    client补充

--- Client/Client/Socket/SANSocket.swift
+++ Client/Client/Socket/SANSocket.swift
@@ -8,22 +8,22 @@
 
 import UIKit
 
-protocol SANSocketDelegate : class {
-    func socket(_ socket : SANSocket, joinRoom user : UserInfo)
-    func socket(_ socket : SANSocket, leaveRoom user : UserInfo)
-    func socket(_ socket : SANSocket, chatMsg : ChatMessage)
-    func socket(_ socket : SANSocket, giftMsg : GiftMessage)
+enum MessageType : Int {
+    case join = 0
+    case leave = 1
+    case text = 2
+    case gift = 3
 }
 
-class SANSocket {
-    weak var delegate : SANSocketDelegate?
+class SANSocket: NSObject {
     fileprivate var tcpClient : TCPClient
-    fileprivate var userInfo : UserInfo.Builder = {
-        let userInfo = UserInfo.Builder()
-        userInfo.name = "why\(arc4random_uniform(10))"
-        userInfo.level = 20
-        userInfo.iconUrl = "icon\(arc4random_uniform(5))"
-        return userInfo
+    fileprivate var isConnected : Bool = false
+    fileprivate lazy var user : UserInfo.Builder = {
+        let user = UserInfo.Builder()
+        user.level = Int32(arc4random_uniform(20))
+        user.name = "why\(arc4random_uniform(10))"
+        user.iconUrl = "icon\(arc4random_uniform(2))"
+        return user
     }()
     
     init(addr : String, port : Int) {
@@ -31,121 +31,120 @@ class SANSocket {
     }
 }
 
-
 extension SANSocket {
-    func connectServer() -> Bool {
-        return tcpClient.connect(timeout: 5).0
-    }
     
+    func connectServer(_ timeout : Int) -> Bool {
+        isConnected = true
+        return tcpClient.connect(timeout: timeout).0
+    }
     
     func startReadMsg() {
         DispatchQueue.global().async {
-            while true {
-                guard let lMsg = self.tcpClient.read(4) else {
-                    continue
-                }
-                // 1.读取长度的data
-                let headData = Data(bytes: lMsg, count: 4)
-                var length : Int = 0
-                (headData as NSData).getBytes(&length, length: 4)
-                
-                // 2.读取类型
-                guard let typeMsg = self.tcpClient.read(2) else {
-                    return
-                }
-                let typeData = Data(bytes: typeMsg, count: 2)
-                var type : Int = 0
-                (typeData as NSData).getBytes(&type, length: 2)
-                print(type)
-                
-                // 2.根据长度, 读取真实消息
-                guard let msg = self.tcpClient.read(length) else {
-                    return
-                }
-                let data = Data(bytes: msg, count: length)
-                
-                // 3.处理消息
-                DispatchQueue.main.async {
-                    self.handleMsg(type: type, data: data)
+            while self.isConnected {
+                // 1.取出长度消息
+                if let lengthMsg = self.tcpClient.read(4) {
+                    let lData = Data(bytes: lengthMsg, count: 4)
+                    var length : Int = 0
+                    (lData as NSData).getBytes(&length, length: 4)
+                    
+                    // 2.读取类型消息
+                    guard let typeMsg = self.tcpClient.read(2) else {
+                        continue
+                    }
+                    
+                    var type : Int = 0
+                    let tdata = Data(bytes: typeMsg, count: 2)
+                    (tdata as NSData).getBytes(&type, length: 2)
+                    
+                    // 3.读取消息
+                    guard let msg = self.tcpClient.read(length) else {
+                        continue
+                    }
+                    let msgData = Data(bytes: msg, count: length)
+                    
+                    // 4.消息转发出去
+                    DispatchQueue.main.async {
+                        self.handleMsg(type, msgData: msgData)
+                    }
                 }
             }
         }
     }
     
-    fileprivate func handleMsg(type : Int, data : Data) {
+    fileprivate func handleMsg(_ type : Int, msgData : Data) {
         switch type {
         case 0, 1:
-            let user = try! UserInfo.parseFrom(data: data)
-            type == 0 ? delegate?.socket(self, joinRoom: user) : delegate?.socket(self, leaveRoom: user)
+            let user = try! UserInfo.parseFrom(data: msgData)
+            print(user.name, user.level, user.iconUrl)
         case 2:
-            let chatMsg = try! ChatMessage.parseFrom(data: data)
-            delegate?.socket(self, chatMsg: chatMsg)
+            let chatMsg = try! ChatMessage.parseFrom(data: msgData)
+            print(chatMsg.text)
+            print(chatMsg.user.name, chatMsg.user.level, chatMsg.user.iconUrl)
         case 3:
-            let giftMsg = try! GiftMessage.parseFrom(data: data)
-            delegate?.socket(self, giftMsg: giftMsg)
+            let giftMsg = try! GiftMessage.parseFrom(data: msgData)
+            print(giftMsg.giftUrl, giftMsg.giftname, giftMsg.giftcount)
+            print(giftMsg.user.name, giftMsg.user.level, giftMsg.user.iconUrl)
         default:
-            print("未知类型")
+            print("其他类型消息")
         }
     }
-}
-
-
-
-extension SANSocket {
-    func sendJoinRoom() {
-        // 1.获取消息的长度
-        let msgData = (try! userInfo.build()).data()
+    
+    func sendJoinMsg() {
+        // 1.获取长度
+        let data = (try! user.build()).data()
         
         // 2.发送消息
-        sendMsg(data: msgData, type: 0)
+        sendMsg(type: 0, msgData: data)
     }
     
-    func sendLeaveRoom() {
-        // 1.获取消息的长度
-        let msgData = (try! userInfo.build()).data()
+    func sendLeaveMsg() {
+        // 1.获取长度
+        let data = (try! user.build()).data()
         
         // 2.发送消息
-        sendMsg(data: msgData, type: 1)
+        sendMsg(type: 1, msgData: data)
     }
     
-    func sendTextMsg(message : String) {
-        // 1.创建TextMessage类型
+    func sendTextMsg(_ text : String) {
+        // 1.创建聊天的对象
         let chatMsg = ChatMessage.Builder()
-        chatMsg.text = message
+        chatMsg.text = text
+        chatMsg.user = try! user.build()
         
-        // 2.获取对应的data
+        // 2.转成data类型
         let chatData = (try! chatMsg.build()).data()
         
-        // 3.发送消息到服务器
-        sendMsg(data: chatData, type: 2)
+        // 3.发送消息
+        sendMsg(type: 2, msgData: chatData)
     }
     
-    func sendGiftMsg(giftName : String, giftURL : String, giftCount : Int) {
-        // 1.创建GiftMessage
+    func sendGiftMsg(_ giftname : String, _ giftURL : String, _ giftcount : Int) {
+        // 1.创建礼物的对象
         let giftMsg = GiftMessage.Builder()
-        giftMsg.giftname = giftName
+        giftMsg.giftname = giftname
         giftMsg.giftUrl = giftURL
-        giftMsg.giftcount = Int32(giftCount)
+        giftMsg.giftcount = Int32(giftcount)
+        giftMsg.user = try! user.build()
         
-        // 2.获取对应的data
+        // 2.转成data类型
         let giftData = (try! giftMsg.build()).data()
         
-        // 3.发送礼物消息
-        sendMsg(data: giftData, type: 3)
+        // 3.发送消息
+        sendMsg(type: 3, msgData: giftData)
     }
     
-    func sendMsg(data : Data, type : Int) {
-        // 1.将消息长度, 写入到data
-        var length = data.count
-        let headerData = Data(bytes: &length, count: 4)
+    func sendMsg(type : Int, msgData : Data) {
+        // 1.长度data
+        var length = msgData.count
+        let lengthData = Data(bytes: &length, count: 4)
         
-        // 2.消息类型
-        var tempType = type
-        let typeData = Data(bytes: &tempType, count: 2)
+        // 2.类型data
+        var type = type
+        let typeData = Data(bytes: &type, count: 2)
         
-        // 3.发送消息
-        let totalData = headerData + typeData + data
-        tcpClient.send(data: totalData)
+        // 3.拼接所有的data,并且发送消息
+        let totalData = lengthData + typeData + msgData
+        _ = tcpClient.send(data: totalData)
     }
 }
 
--- Client/Client/ViewController.swift
+++ Client/Client/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import ProtocolBuffers
 
 class ViewController: UIViewController {
-
+    
+    //    fileprivate lazy var tcpClient : TCPClient = TCPClient(addr: "192.168.1.100", port: 7999)
+    fileprivate lazy var socket : SANSocket = SANSocket(addr: "192.168.0.104", port: 7999)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if socket.connectServer(10) {
+            print("连接成功")
+            socket.startReadMsg()
+        }
+        
+        /*
+         if tcpClient.connect(timeout: 5).0 {
+         let p = Person.Builder()
+         p.id = 19880101
+         p.name = "coderwhy"
+         p.email = "372623326@qq.com"
+         
+         let pdata = (try! p.build()).data()
+         
+         // 1.用户记录长度的data
+         var length = pdata.count
+         let lengthData = Data(bytes: &length, count: 4)
+         //            _ = tcpClient.send(data: lengthData)
+         
+         // 2.发哦少年宫类型
+         var type = 2
+         let typeData = Data(bytes: &type, count: 2)
+         //            _ = tcpClient.send(data: typeData)
+         
+         // 3.发送真实消息
+         let data = lengthData + typeData + pdata
+         _ = tcpClient.send(data: data)
+         }
+         */
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func btnClick(_ sender: UIButton) {
+        
+        switch sender.tag {
+        case 0:
+            socket.sendJoinMsg()
+        case 1:
+            socket.sendLeaveMsg()
+        case 2:
+            socket.sendTextMsg("你好")
+        case 3:
+            socket.sendGiftMsg("服务商", "http://www.abc.com", 1000)
+        default:
+            print("未识别消息")
+        }
     }
-
-
 }
+
+
 
